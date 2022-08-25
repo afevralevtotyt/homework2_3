82b8f41bf1b9dc5d5b5a1d8e9e65abdb4f594ff2
Anton <afevraleff@ya.ru>
2022-08-25 13:40:14 +0200

    saving

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1(){
+        int i = 100000;
+        byte b = 123;
+        short s = 13412;
+        long l = 123455454L;
+        float f = 123.1f;
+
+
+    }
+}
